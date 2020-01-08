98c0a4b1855414aea9684540528909b8c3f77011
Lucas Haefner <lucashaefner360@gmail.com>
2020-01-08 15:22:48 -0500

    Updated filesystem

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
+import setuptools
 
-from distutils.core import setup
-setup(
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
   name = 'StrFry',
-  packages = ['StrFry'],
-  version = '0.1',
+  version = '0.0.1',
   license='MIT',
   description = 'The easiest way to create human-readable tables in Python!',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = 'Lucas Haefner',
   author_email = 'lucashaefner360@gmail.com',
   url = 'https://github.com/LucasHaefner/StrFry',
   download_url = 'https://github.com/LucasHaefner/StrFry/archive/v_01.tar.gz',
   keywords = ['String', 'Str', 'Dunder', 'Special Method', 'Table', 'Tables', 'Data'],
-  install_requires=[],
+  packages = setuptools.find_packages(),
   classifiers=[
+    'Programming Language :: Python :: 3',
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+    'Operating System :: OS Independent',
   ],
+  python_requires='>=3.6',
 )
