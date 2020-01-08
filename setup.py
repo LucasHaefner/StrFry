a80b251dd89e0aa37a6d92230b172454de539479
Lucas Haefner <lucashaefner360@gmail.com>
2020-01-08 15:01:55 -0500

    Added setup files for pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from distutils.core import setup
+setup(
+  name = 'StrFry',
+  packages = ['StrFry'],
+  version = '0.1',
+  license='MIT',
+  description = 'The easiest way to create human-readable tables in Python!',
+  author = 'Lucas Haefner',
+  author_email = 'lucashaefner360@gmail.com',
+  url = 'https://github.com/LucasHaefner/StrFry',
+  download_url = 'https://github.com/LucasHaefner/StrFry/archive/v_01.tar.gz',
+  keywords = ['String', 'Str', 'Dunder', 'Special Method', 'Table', 'Tables', 'Data'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
