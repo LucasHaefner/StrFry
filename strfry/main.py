51526f0d95c7653c4029494b21cf4b5872bbc855
Lucas Haefner <lucashaefner360@gmail.com>
2020-01-11 14:34:53 -0500

    Implement new 'separator' system
    -separator is a list of strings
    -first character '\' in strings indicates indent
    -__init__ cleaned up
    -separate() removed

--- strfry/main.py
+++ strfry/main.py
@@ -1,31 +1,14 @@
 # ...StrFry/main.py
 
 class Table:
-    def __init__(self, table, separators=[{':':False}], grouping='rows'):
+    def __init__(self, table, separators=[':'], grouping='rows'):
         self.table = table
         self.separators = separators
         self.grouping = grouping
         errors = {'grouping':f'Invalid groupinging key \'{self.grouping}\'. Use \'rows\' or \'columns\'.'}
         
-        if self.grouping == 'rows':
-            if len(self.separators) < len(self.table):
-                for _ in range(len(self.table) - 2):
-                    self.separators.append(separators[:1][0])
-        elif self.grouping == 'columns':
-            if len(self.separators) < len(self.flip()):
-                for _ in range(len(self.flip()) - 2):
-                    self.separators.append(separators[:1][0])
-        else:
+        if self.grouping != 'rows' and self.grouping != 'columns':
             raise ValueError(errors[grouping])
-
-    def separate(self, chars, alignments):
-        """
-        Makes it easier to find the correct character to separate later on.
-        :param chars: iterable of single characters
-        :param alignments: iterable of boolean values :True: center :False: left
-        :return: list of dicts :key: char :value: alignment
-        """
-        return [dict(zip(chars(i), alignments(i))) for i in range(min(len(chars), len(alignment)))]
 
     def normalize(self):
         """
@@ -55,14 +38,18 @@ class Table:
         :retur:s string with left justified elements formatted with a separator
         """
         string = ''
+        array, width, alignments = [], [], []
         margin = 1
         padding = margin * ' '
-        array = []
-        width = []
-        alignments = []
 
-        [alignments.append(bool(list(i.values())[0])) for i in self.separators]
         [width.append(len(max(s, key=len))) for s in self.table]
+
+        for i in self.separators:
+            if i[0] == '/' and len(i) > 1:
+                alignments.append(True)
+                i = i[1:]
+            else:
+                alignments.append(False)
 
         for s in self.table:
             slot = []
